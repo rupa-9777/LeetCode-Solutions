3c1f5311336c3af115fb7b932d70e3200e548af0
rupa-9777 <rupasreeputta9999@gmail.com>
2026-09-13 21:46:27 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.22%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n= s.size()-1;
+        while(n>=0 && s[n]==' ')
+         n--;
+        int cnt =0;
+        while(n>=0 && s[n]!=' '){
+         cnt++;
+         n--;
+        }
+    return cnt;
+    }
+};
