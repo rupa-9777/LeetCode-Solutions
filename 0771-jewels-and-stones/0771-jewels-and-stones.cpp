5c66626d95d7b87e16ca370772af8156b3830614
rupa-9777 <rupasreeputta9999@gmail.com>
2026-09-17 23:47:05 +0530

    Time: 0 ms (100%), Space: 8.3 MB (78.57%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0;
+
+        for (char s : stones) {
+            if (jewels.find(s) != string::npos) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}; 
+   
